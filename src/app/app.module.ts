daecea2bb05c86364dc2683b4fe32f484cccdee1
carlacentenor <carlacentenor@gmail.com>
2021-03-04 09:48:03 -0500

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
+import { NewPasswordComponent } from './new-password/new-password.component';
+import { ListUsersComponent } from './list-users/list-users.component';
+import { EditUsersComponent } from './edit-users/edit-users.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ComprobantesComponent } from './comprobantes/comprobantes.component';
+import { ConfigSystemComponent } from './config-system/config-system.component';
+import { NewUserComponent } from './new-user/new-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ChangePasswordComponent,
+    NewPasswordComponent,
+    ListUsersComponent,
+    EditUsersComponent,
+    DashboardComponent,
+    ComprobantesComponent,
+    ConfigSystemComponent,
+    NewUserComponent
   ],
   imports: [
     BrowserModule,
